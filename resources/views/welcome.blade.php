b3a760ae03bdf46a882fb6efb9d69877e2fe699e
m-n-sakib-dev <mdsakib.ndc17@gmail.com>
2026-04-05 11:44:22 +0600

    registration added

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2167,7 +2167,7 @@
 </head>
 
 <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-    server connected
+    <h1 class="text-white">server updated</h1>
     <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
         @if (Route::has('login'))
         <nav class="flex items-center justify-end gap-4">
